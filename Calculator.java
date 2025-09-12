fecd9534b3fbc581db41dba5ca43d478da7b95f7
heoyoonah <jimin0875@gmail.com>
2025-09-12 12:12:19 +0900

    0912 과제: 덧셈 뺼셈

--- Calculator.java
+++ Calculator.java
@@ -14,17 +14,5 @@ public class Calculator {
         // 빼기 연산
         double sub = a - b;
         System.out.println(a + " - " + b + " = " + sub);
-
-        // 곱하기 연산
-        double mul = a * b;
-        System.out.println(a + " × " + b + " = " + mul);
-        
-        // 나누기 연산
-        if (b != 0) {
-            double div = a / b;
-            System.out.println(a + " ÷ " + b + " = " + div);
-        } else {
-            System.out.println("나누기: 0으로 나눌 수 없습니다.");
-        }
     }
 }
