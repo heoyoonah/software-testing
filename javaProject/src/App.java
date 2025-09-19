7f8d99e70b11511af92beb14639817cc1e1de3f8
heoyoonah <jimin0875@gmail.com>
2025-09-19 12:03:21 +0900

    0919 java test

---
+++ javaProject/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(sayHello());
+    }
+
+
+    public static String sayHello() {
+        return "hello, world";
+    }
+}
---
+++ javaProject/src/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class AppTest {
+    @Test
+    public void testHello() {
+        assertEquals("hello, world", App.sayHello());
+    }
+
+
+}
